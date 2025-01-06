e2c864daddd2de59b8e4f6b7aee09dd903d2d4cb
OxumLabs <oxumlabs@proton.me>
2025-01-06 18:35:33 +0530

    neit lsp for vscode

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# ✨ VS Code Gleam ✨
+#### Neit Language Extension
 
-[![Visual Studio Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/Gleam.gleam?label=Visual%20Studio%20Marketplace&logo=visual-studio-code)](https://marketplace.visualstudio.com/items?itemName=Gleam.gleam)
+> A huge thanks to [Gleam VSCode Extension](https://github.com/gleam-lang/vscode-gleam) for providing the base for this extension! This extension is a modification of the original Gleam VSCode extension and is intended for the Neit language.
 
-Provides syntax highlighting and use of the Gleam Language Server, which itself
-provides formatting, goto-definition, autocompletion, and more!
+# Modified by Joy on 2/11/24 7:12 PM
 
-## Hacking on the extension
+---
 
-Run `npm run compile-watch` in a terminal and hit F5 in vscode.
+## License
 
-Contribute to the extension on [Github](https://github.com/gleam-lang/vscode-gleam)
+This project is based on the [Gleam VSCode Extension](https://github.com/gleam-lang/vscode-gleam), which is licensed under the **Apache License 2.0**. Please refer to the `LICENSE` file for more details.
+
+Changes made to the original code are subject to the terms of the Apache 2.0 License. Any contributions you make to this repository will also be licensed under the Apache 2.0 License.
+
+---
+
+## Notice
+
+- The name "Neit" is used in place of "Gleam" to reflect the focus of this extension for the Neit language.
+- You are free to use, modify, and distribute this extension, but the Apache 2.0 License applies to both the original and modified parts of this code.
+
+---
+
+## Acknowledgments
+
+- [Gleam](https://gleam.run/): The programming language the VSCode LSP was orginally based on
--- src/extension.ts
+++ src/extension.ts
@@ -1,165 +1,65 @@
-import * as fs from "fs";
-import * as path from "path";
 import * as vscode from "vscode";
-import { workspace } from "vscode";
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-} from "vscode-languageclient/node";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
 
-const enum GleamCommands {
-  RestartServer = "gleam.restartServer",
+const enum NeitCommands {
+  RestartServer = "neit.restartServer",
 }
 
 let client: LanguageClient | undefined;
-let configureLang: vscode.Disposable | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
-  const onEnterRules = [...continueTypingCommentsOnNewline()];
-
-  configureLang = vscode.languages.setLanguageConfiguration("gleam", {
-    onEnterRules,
-  });
-
+  // Register restart command
   const restartCommand = vscode.commands.registerCommand(
-    GleamCommands.RestartServer,
+    NeitCommands.RestartServer,
     async () => {
       if (!client) {
-        vscode.window.showErrorMessage("gleam client not found");
+        vscode.window.showErrorMessage("Neit client not found");
         return;
       }
 
       try {
         if (client.isRunning()) {
           await client.restart();
-
-          vscode.window.showInformationMessage("gleam server restarted.");
+          vscode.window.showInformationMessage("Neit server restarted.");
         } else {
           await client.start();
         }
       } catch (err) {
         client.error("Restarting client failed", err, "force");
       }
-    },
+    }
   );
 
   context.subscriptions.push(restartCommand);
 
+  // Create and start the Neit language client (LSP server)
   client = await createLanguageClient();
-  // Start the client. This will also launch the server
   client?.start();
 }
 
-// this method is called when your extension is deactivated
 export function deactivate(): Thenable<void> | undefined {
-  configureLang?.dispose();
-
   return client?.stop();
 }
 
 async function createLanguageClient(): Promise<LanguageClient | undefined> {
-  const command = await getGleamCommandPath();
-  if (!command) {
-    const message = `Could not resolve Gleam executable. Please ensure it is available
-    on the PATH used by VS Code or set an explicit "gleam.path" setting to a valid Gleam executable.`;
-
-    vscode.window.showErrorMessage(message);
-    return;
-  }
+  const command = "neit"; // Using the "neit" command directly for the LSP server
 
   const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "gleam" }],
-    synchronize: {
-      fileEvents: [
-        workspace.createFileSystemWatcher("**/gleam.toml"),
-        workspace.createFileSystemWatcher("**/manifest.toml"),
-      ],
-    },
+    documentSelector: [{ scheme: "file", language: "neit" }],
   };
 
   const serverOptions: ServerOptions = {
     command,
-    args: ["lsp"],
+    args: ["lsp"], // Assuming "lsp" is the argument to start the server
     options: {
-      env: Object.assign(process.env, {
-        GLEAM_LOG: "info",
-        GLEAM_LOG_NOCOLOUR: "1",
-      }),
+      env: { ...process.env, NEIT_LOG: "info", NEIT_LOG_NOCOLOUR: "1" },
     },
   };
 
   return new LanguageClient(
-    "gleam_language_server",
-    "Gleam Language Server",
+    "neit_language_server",
+    "Neit Language Server",
     serverOptions,
-    clientOptions,
+    clientOptions
   );
-}
-
-/**
- * Returns the `OnEnterRule`s needed to configure typing comments on a newline.
- *
- * This makes it so when you press `Enter` while in a comment it will continue
- * the comment onto the next line.
- */
-function continueTypingCommentsOnNewline(): vscode.OnEnterRule[] {
-  const indentAction = vscode.IndentAction.None;
-
-  return [
-    {
-      // Module doc single-line comment
-      // e.g. ////|
-      beforeText: /^\s*\/{4}.*$/,
-      action: { indentAction, appendText: "//// " },
-    },
-    {
-      // Doc single-line comment
-      // e.g. ///|
-      beforeText: /^\s*\/{3}.*$/,
-      action: { indentAction, appendText: "/// " },
-    },
-  ];
-}
-
-/** Returns the absolute path to a gleam command. */
-export async function getGleamCommandPath(): Promise<string | undefined> {
-  const command = getWorkspaceConfigGleamExePath();
-  const workspaceFolders = vscode.workspace.workspaceFolders;
-  if (!command || !workspaceFolders) {
-    return command ?? "gleam";
-  } else if (!path.isAbsolute(command)) {
-    // if sent a relative path, iterate over workspace folders to try and resolve.
-    for (const workspace of workspaceFolders) {
-      const commandPath = path.resolve(workspace.uri.fsPath, command);
-      if (await fileExists(commandPath)) {
-        return commandPath;
-      }
-    }
-    return undefined;
-  }
-  return command;
-}
-
-const EXTENSION_NS = "gleam";
-
-function getWorkspaceConfigGleamExePath(): string | undefined {
-  const exePath = vscode.workspace.getConfiguration(EXTENSION_NS).get("path");
-  // it is possible for the path to be blank. In that case, return undefined
-  if (typeof exePath !== "string" || !exePath || exePath.trim().length === 0) {
-    return undefined;
-  } else {
-    return exePath;
-  }
-}
-
-function fileExists(executableFilePath: string): Promise<boolean> {
-  return new Promise<boolean>((resolve) => {
-    fs.stat(executableFilePath, (err, stat) => {
-      resolve(err == null && stat.isFile());
-    });
-  }).catch(() => {
-    // ignore all errors
-    return false;
-  });
 }
